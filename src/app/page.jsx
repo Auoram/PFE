2b7acc38b7a568f9b69d2a877c3dceee08af3686
Auoram <marouaboumchich@gmail.com>
2024-02-06 13:15:18 +0100

    first try

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import Image from "next/image";
+import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from "react";
+
+export default function Home() {
+  return (
+    <div className="try">
+    home
+    </div>
+  );
+}
